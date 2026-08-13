05769e8a6b3194c6b40564a2132443442ae648c1
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-08-13 11:28:29 +0530

    learn about inersection types

--- Interface and Type Alias/src/app.ts
+++ Interface and Type Alias/src/app.ts
@@ -17,8 +17,39 @@ const getAdmin=(obj:Admin)=>{
     return obj
 }
 
-// let userInfo=getUser({name:"nikhi",email:"bishnoi@email.com",password:"23232323232444"})
+let userInfo=getUser({name:"nikhi",email:"bishnoi@email.com",password:"23232323232444"})
 let adminInfo=getAdmin({name:"nikhi",email:"bishnoi@email.com",password:"23232323232444",admin:false})
 
 console.log(adminInfo)
 
+
+type custom=Number|String|boolean
+
+let a:custom;
+let b:custom;
+
+ a="nikhil"
+ b=false
+
+console.log(typeof a)
+console.log(typeof b)
+
+
+interface Person{
+    name:String,
+    age:Number
+}
+
+interface Employee{
+    role:String;
+    slary:Number
+}
+
+let staff:Person&Employee={
+    name:"nikhil",
+    age:20,
+    role:"backend-developer",
+    slary:50000
+}
+
+console.log(staff)
