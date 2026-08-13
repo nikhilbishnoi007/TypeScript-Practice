da2cce5795d3c8573b048c9282a1e03b0fc1f674
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-08-13 14:45:46 +0530

    crate a construcotr

--- OOPs-Concepts/src/app.ts
+++ OOPs-Concepts/src/app.ts
@@ -1,12 +1,23 @@
 class Device{
-    name="Realme";
-    price=20000;
-    model="narzo 50 5g"
+    brand:string;
+    price:number;
+    model:string
+    constructor (  brand:string , price:number,  model:string){
+        this.brand=brand;
+        this.price=price;
+        this.model=model;
+    }
+    deviceInfo(){
+      console.log(`brand_name:${this.brand},device_model:${this.model},device_price: ${this.price}`)
+       
+  
+    }
+  
 
 }
 
-let d1=new Device
+let d1=new Device("realme",20000,"narzo50 5g")
+d1.deviceInfo()
 
-console.log(d1)
 
 
