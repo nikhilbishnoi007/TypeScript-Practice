29e18f3721f74424f1644ac1ef4a78a287dd5af5
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-08-13 15:09:03 +0530

    remove a unwanted change

--- OOPs-Concepts/src/app.ts
+++ OOPs-Concepts/src/app.ts
@@ -1,23 +1,23 @@
 class Device{
     brand:string;
     price:number;
-    model:string
+    model:string;
     constructor (  brand:string , price:number,  model:string){
         this.brand=brand;
         this.price=price;
-        this.model=model;
+        this.model=model
     }
     deviceInfo(){
-      console.log(`brand_name:${this.brand} ,device_model:${this.model}, device_price: ${this.price}`)
-       
-  
+      console.log(`brand_name:${this.brand} ,device_model:${this.model}, device_price: ${this.price}`)  
     }
   
 
 }
 
 let d1=new Device("iphone",70000,"iphone 15")
+let d2=new Device("google",65000,"pixel 7 ")
 d1.deviceInfo()
+d2.deviceInfo()
 
 
 
