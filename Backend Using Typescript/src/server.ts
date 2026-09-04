2b20efc0fa7aaf95f3de1868f51840c1700dc0dd
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-09-04 11:30:24 +0530

    error handling

--- Backend Using Typescript/src/server.ts
+++ Backend Using Typescript/src/server.ts
@@ -1,11 +1,16 @@
 import { app } from "./app";
 import connectDb from "./config/db";
 import config from "./config/config";
+import { error } from "node:console";
 
 const port=config.PORT
 
-connectDb()
-
-app.listen(port,()=>{
+connectDb().then(()=>{
+ app.listen(port,()=>{
     console.log(`server is listening at port:${port}`)
 })
+})
+.catch((error)=>{
+    console.log("mongo db connection failed")
+})
+
