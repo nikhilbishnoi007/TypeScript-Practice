115fb28030cc46b6f5e4db9d0ed85a5195b5b69c
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-09-04 14:56:37 +0530

    create utils

--- Backend Using Typescript/src/app.ts
+++ Backend Using Typescript/src/app.ts
@@ -6,7 +6,7 @@ import authRouter from "./routes/auth.routes";
 
 const app=express()
 
-app.use(express.json())
+app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 app.use(CookieParser())
