ffe5390571eb70cc31be989866bda6e7c95812c7
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-09-10 10:56:06 +0530

    "delete utils"

--- Backend Using Typescript/src/utils/asyncHnadler.ts
+++
@@ -1,8 +0,0 @@
-import {Request,Response,NextFunction,RequestHandler} from "express"
-
-const asyncHandler=(requestHandler:RequestHandler)=>{
-           return (req:Request,res:Response,next:NextFunction)=>{
-                Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-            }
-}
-export default asyncHandler
